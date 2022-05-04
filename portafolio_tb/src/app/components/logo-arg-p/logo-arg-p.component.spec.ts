e86d9d61e2bd29295998acdd87b783af9af15a80
tombazan <tomasbazan95@gmail.com>
2022-05-04 18:24:55 -0300

    front commit
    Commit de lo que tengo hasta el momento.

---
+++ portafolio_tb/src/app/components/logo-arg-p/logo-arg-p.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoArgPComponent } from './logo-arg-p.component';
+
+describe('LogoArgPComponent', () => {
+  let component: LogoArgPComponent;
+  let fixture: ComponentFixture<LogoArgPComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogoArgPComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogoArgPComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
